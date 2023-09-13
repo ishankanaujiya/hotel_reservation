b407e3b624d0f4042a022c3c4e2c4884b22818ef
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2023-09-13 22:14:30 +0545

    Error occured

--- src/authentication.c
+++ src/authentication.c
@@ -12,7 +12,7 @@ int i = 0, value = 0, j = 0, choice = 0, count = 0, count1 = 0;
 char ch, ch1, username[100], pass[100], password_map_char[8];
 
 // Variables for Password Check
-int char_string_length, count_password_check, numbers_password = 0, alphabets_password = 0, special_password = 0;
+//int char_string_length, count_password_check, numbers_password = 0, alphabets_password = 0, special_password = 0;
 
 
 void registrationPage()
@@ -62,78 +62,12 @@ void registrationPage()
 	fflush(stdin);
 	printf("\n                :--------------------------------------------:");
 
-	// Strong Password Condition
-	/*system("cls");
-	printf("\n\n");
-	system("color 0B");
-	printf("\n *********************************************************************");
-	printf("\n\t Please Enter a Strong Password.");
-	printf("\n\t Process To create a Strong Password");
-	printf("\n\t 1. Maximum Charater 8.");
-	printf("\n\t 2. Must Contain atleast one Chapital Letter");
-	printf("\n\t 3. Must contain Special Characters '@', '#', '$', '*', '&'");
-	printf("\n *********************************************************************");
-	Sleep(1000);
-	system("color 07");
-	printf("\n\n");*/
-
-password_flag:
-
-	printf("\n                :--------------------------------------------:");
 	fflush(stdin);
 	printf("\n                        :Password: ");
 	gets(form.password);
-	//strcpy(password_map_char, form.password);
 	printf("\n                :---------------------------------------------");
 
-	//char_string_length = strlen(form.password);
-
-	/*if (char_string_length > 8)
-	{
-		printf("\n\n\n ****************************************************************");
-		printf("\n   Too many characters... Please Enter max 8 Characters....");
-		printf("\n ****************************************************************");
-		printf("\n");
-		Sleep(1000);
-		// system("color 07");
-
-		goto password_flag;
-	}
-
-	numbers_password = 0;
-	alphabets_password = 0;
-	special_password = 0;
 	
-	for (i = 0; i < 8; i++)
-	{
-		if (password_map_char[i] >= 48 && password_map_char[i] <= 57)
-		{
-			numbers_password = 1;
-		}
-		if (password_map_char[i] >= 'A' && password_map_char[i] <= 'Z')
-		{
-			alphabets_password = 1;
-		}
-		if (password_map_char[i] == '@' || password_map_char[i] == '#' || password_map_char[i] == '$' || password_map_char[i] == '*')
-		{
-			special_password = 1;
-		}
-	}
-
-	if (numbers_password == 1 && alphabets_password == 1 && special_password == 1)
-	{
-		printf("\n Strong Password");
-	}
-	else
-	{
-		printf("\n\n\n **********************************************");
-		printf("\n The Entered Password is not Strong");
-		printf("\n Please Enter a Password Again");
-		printf("\n **********************************************");
-		printf("\n");
-		Sleep(1000);
-		goto password_flag;
-	}*/
 
 	fwrite(&form, sizeof(struct registration_structure), 1, fileLogin);
 	printf("\n Account Regestered Successfully.....\n\n");
@@ -157,7 +91,6 @@ int loginPage()
 	}
 	rewind(fileLogin);
 	fread(&login, sizeof(login), 1, fileLogin);
-	//fread(&password_check, sizeof(struct registration_structure), 1,fileLogin);
 	printf("\n                       :------------------------------------------------------:");
 	printf("\n                       :    :---------------------------------------------:   :");
 	printf("\n                       :    :                   LOGIN                     :   :");
@@ -220,12 +153,14 @@ int loginPage()
 		printf("\n ------------------------------------------------------------------------------------");
 		printf("\n  Wrong Username or Password");
 		printf("\n ------------------------------------------------------------------------------------\n");
+		fclose(fileLogin);
 		return 0;
 		Sleep(1000);
 	}
 	else
 	{
 	//	i=0;
+		fclose(fileLogin);
 		return 1;
 	}
 	//fclose(fileLogin);
